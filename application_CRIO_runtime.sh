375da4c9992d68e342967d4ba60dbcebc1289782
zhuang ma <zhuangzhimhao@yahoo.com>
2021-07-02 16:06:13 +0800

    modified:   application_CRIO_runtime.sh 	modified:   application_kubernetes.sh

---
+++ application_CRIO_runtime.sh
@@ -0,0 +1,16 @@
+#If installing cri-o-runc (recommended), you'll need to install libseccomp >= 2.4.1.
+#This is not available in distros based on Debian buster or below, so buster backports will need to be enabled:
+echo 'deb http://deb.debian.org/debian buster-backports main' > /etc/apt/sources.list.d/backports.list
+apt update
+apt install -y libseccomp2 || apt update -y libseccomp2
+
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > \
+    /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" > \
+    /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
+
+apt-get update
+apt-get install cri-o cri-o-runc
--- application_kubernetes.sh
+++ application_kubernetes.sh
@@ -1,12 +1,44 @@
+#####################################################################################################################
+#
+#####################################################################################################################
+#Kubernetes consists of blow module:
+#Control-plane node(s):
+Protocol	Direction	Port Range	Purpose	                Used By
+TCP	        Inbound	    6443*	    Kubernetes API server	All
+TCP	        Inbound	    2379-2380   etcd server client API	kube-apiserver, etcd
+TCP	        Inbound	    10250	    kubelet API	            Self, Control plane
+TCP	        Inbound	    10251	    kube-scheduler	        Self
+TCP	        Inbound	    10252	    kube-controller-manager	Self
+#Worker node(s):
+Protocol	Direction	Port Range	Purpose	                Used By
+TCP	        Inbound	    10250	    kubelet API	            Self, Control plane
+TCP	        Inbound	    30000-32767	NodePort Services       All
+
+#####################################################################################################################
+#
+#####################################################################################################################
 #Install Kubernetes on debian10.
-#System required: 2 CPU or more, disable SWAP, 2GB or more RAM.
-#Backup source configuration file.
-cp /etc/apt/sources.list /etc/apt/source.list.bak
-#Add Kubernetes source.
-printf "deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" >> /etc/apt/source.list
+#System required:
+2 CPU or more, disable SWAP, 2GB or more RAM.
 #Install tools required.
+apt-get update
 apt-get install curl
-apt-get install
-apt-get install
-apt-get install
-apt-get install
+apt-get install gnupg
+apt-get install apt-transport-https
+apt-get install ca-certificates
+apt-get install curl
+
+#Update apt source list.
+#Backup source configuration file:
+cp /etc/apt/sources.list /etc/apt/source.list.bak
+#Download the Google Cloud public signing key:
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+#Add the Kubernetes apt repository:
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
+    | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#Add aliyun source:
+#printf "deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" >> /etc/apt/source.list
+
+#Update apt package index with the new repository and install kubectl:
+sudo apt-get update
+sudo apt-get install -y kubectl
