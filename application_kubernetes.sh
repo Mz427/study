d674e562280bf9894f7854a6167f0950bea656e0
zhuang ma <zhuangzhimhao@yahoo.com>
2021-07-02 11:53:47 +0800

    new file:   application_CRIO_runtime.sh 	new file:   application_kubernetes.sh

---
+++ application_kubernetes.sh
@@ -0,0 +1,12 @@
+#Install Kubernetes on debian10.
+#System required: 2 CPU or more, disable SWAP, 2GB or more RAM.
+#Backup source configuration file.
+cp /etc/apt/sources.list /etc/apt/source.list.bak
+#Add Kubernetes source.
+printf "deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" >> /etc/apt/source.list
+#Install tools required.
+apt-get install curl
+apt-get install
+apt-get install
+apt-get install
+apt-get install
