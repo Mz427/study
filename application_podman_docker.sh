f7fe17a2ecced790d66729001cae81b7930cf21d
Mz Zhuang <zhuangzhimhao@yahoo.com>
2021-09-17 23:31:12 +0800

    modified:   application_podman_docker.sh 	modified:   application_rabbitMQ.sh

--- application_podman_docker.sh
+++ application_podman_docker.sh
@@ -1,12 +1,28 @@
+podman search image_name
+podman pull image_name
+podman [start][stop][restart] container_name
+podman exec [OPTIONS] CONTAINER COMMAND [ARG...]
+podman exec -ti container_name /bin/bash
+podman ps [-a]
 podman run -d -t -i --privileged \
     --restart always \
     --name registry \
     -p 5000:5000 \
     -v /var/lib/registry:/var/lib/registry \
     registry:2
+
 podman run -d -t -i \
     --restart always \
     --name postgresql \
     -p 5432:5432 \
     -e POSTGRES_PASSWORD=929393 \
     postgres:latest
+
+podman run -d \
+    --restart always \
+    --name rabbitmq \
+    -p 5672:5672 \
+    -p 15672:15672 \
+    -e RABBITMQ_DEFAULT_USER=admin \
+    -e RABBITMQ_DEFAULT_PASS=admin \
+    rabbitmq:latest
--- application_rabbitMQ.sh
+++ application_rabbitMQ.sh
@@ -1,11 +1,21 @@
-#Producer:                              Message1 Message2 Message3
-#                                          |        |        |
-#                                          |        |        |
-#Broker:                                   |        |        |
-#    Virtual Host:                         |        |        |
-#        Exchange(direct/fanout/topic):    |        |        |
-#            Bindings:                     |        |        |
-#            Queues:                    Queues1 Queues2 Queues3
-#                                          |        |        |
-#                                          |        |        |
-#Consumer:                              Message1 Message2 Message3
+###################################################################################################
+#
+###################################################################################################
+Producer:                              Message1 Message2 Message3
+                                          |        |        |
+                                          |        |        |
+Broker:                                   |        |        |
+    Virtual Host:                         |        |        |
+        Exchange(direct/fanout/topic):    |        |        |
+            Bindings:                     |        |        |
+            Queues:                    Queues1 Queues2 Queues3
+                                          |        |        |
+                                          |        |        |
+Consumer:                              Message1 Message2 Message3
+
+port:
+    4369(epmd), 25672(Erlang distribution)
+    5672, 5671(AMQP 0-9-1 without and with TLS)
+    15672 (web management)
+    61613, 61614(STOMP)
+    1883, 8883(MQTT)
