b34845c6d101603649fabc98cd0b487b15469e57
Mz Zhuang <zhuangzhimhao@yahoo.com>
2021-09-17 22:28:37 +0800

    new file:   application_rabbitMQ.sh

---
+++ application_rabbitMQ.sh
@@ -0,0 +1,11 @@
+#Producer:                              Message1 Message2 Message3
+#                                          |        |        |
+#                                          |        |        |
+#Broker:                                   |        |        |
+#    Virtual Host:                         |        |        |
+#        Exchange(direct/fanout/topic):    |        |        |
+#            Bindings:                     |        |        |
+#            Queues:                    Queues1 Queues2 Queues3
+#                                          |        |        |
+#                                          |        |        |
+#Consumer:                              Message1 Message2 Message3
